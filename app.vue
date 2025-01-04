73e95402d3e67ad2b4a172817e42d176d9e48c55
christian-rojas <christian.ici17@gmail.com>
2025-01-04 00:03:26 -0300

    change url to image

--- app.vue
+++ app.vue
@@ -22,18 +22,26 @@ export default {
 <script setup lang="ts">
 useHead({
   title: "Claudio Rojas Alarcon",
-  meta: [{ name: "description", content: "Marca personal de Claudio Rojas Alarcon" }],
+  meta: [
+    { name: "description", content: "Marca personal de Claudio Rojas Alarcon" },
+    { property: "og:title", content: "Claudio Rojas Alarcon" },
+    { property: "og:description", content: "Esta es mi marca personal, dejame contarte de que va" },
+    { property: "og:image", content: "https://claudiorojasalarcon.com/claudio-pic.jpeg" },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://claudiorojasalarcon.com" },
+    { name: "twitter:card", content: "summary_large_image" },
+  ],
 });
-useSeoMeta({
-  title: "Claudio Rojas Alarcon",
-  description: "Marca personal de Claudio Rojas Alarcon",
-  ogTitle: "Claudio Rojas Alarcon",
-  ogDescription: "Esta es mi marca personal, dejame contarte de que va",
-  ogImage: "@/assets/claudio-pic.jpeg",
-  ogType: "website",
-  ogUrl: "https://claudiorojasalarcon.com",
-  twitterCard: "summary_large_image",
-});
+// useSeoMeta({
+//   title: "Claudio Rojas Alarcon",
+//   description: "Marca personal de Claudio Rojas Alarcon",
+//   ogTitle: "Claudio Rojas Alarcon",
+//   ogDescription: "Esta es mi marca personal, dejame contarte de que va",
+//   ogImage: "@/assets/claudio-pic.jpeg",
+//   ogType: "website",
+//   ogUrl: "https://claudiorojasalarcon.com",
+//   twitterCard: "summary_large_image",
+// });
 </script>
 
 <style>
